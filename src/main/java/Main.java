c4b6a5143841b6ad3766560012cc93e3ccc1a93f
Dmitriy Khvatov <dkhvatov@playteam.ru>
2013-12-06 12:34:29 +0400

    added package

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-// does nothing.
-
-
-	public static void main(String[] args) {}
-
-}
---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Main {
+
+// does nothing.
+
+
+    public static void main(String[] args) {
+    }
+
+}
